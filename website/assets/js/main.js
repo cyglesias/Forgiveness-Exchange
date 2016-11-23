708ab3d1088d9a11e71e14a8659117e4bf81bba6
Christina <christinayglesias@gmail.com>
2016-11-23 12:58:19 -0800

    restructured data

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,5 +1,48 @@
-$(document).ready(function(){
+var apologyKey = false;
 
+function loadApology() {
+    var ref = firebase.database().ref("apology");
+    ref.orderByChild("approved").limitToLast(1).once("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            apologyKey = childSnapshot.key;
+            console.log("apologyKey", apologyKey);
+            var childData = childSnapshot.val();
+            console.log(childData);
+            $("#apologyprompt").append(childData.apologytext);
+        });
+    });
+}
+
+function forgive(text) {
+    var timestamp = Date.now();
+    var updates = {};
+    updates['apology/' + apologyKey + '/forgivenesstext'] = text;
+    updates['apology/' + apologyKey + '/approved'] = false;
+    updates['apology/' + apologyKey + '/forgivenesstime'] = timestamp;
+
+    console.log(updates);
+    firebase.database().ref().update(updates, function(err) {
+        $("#step2").show();
+    }); 
+}
+
+function apologize(text) {
+    var apology = firebase.database().ref('apology');  
+    var timestamp = Date.now();
+    var data = {
+        apologytext: text, 
+        forgivenesstext: false,
+        approved: false,
+        apologytime: timestamp,
+        forgivenesstime: timestamp
+    }; 
+    console.log (data);
+    apology.push(data, function() {
+        $("#finished").show();
+    });
+}
+
+$(document).ready(function(){
     var config = {
         apiKey: "AIzaSyDCjhaGBs_X3-n-lu0ek9mN2dKLVvLrEsc",
         authDomain: "forgiveness-exchange.firebaseapp.com",
@@ -7,81 +50,33 @@ $(document).ready(function(){
         storageBucket: "forgiveness-exchange.appspot.com",
         messagingSenderId: "878071377818"
     };
-firebase.initializeApp(config);
-
+    
+    firebase.initializeApp(config);
     $("#step1").hide();
     $("#step2").hide();
     $("#finished").hide();
-
-});
-
- $('#readybutton').click(function() {
+    $('#readybutton').click(function() {
         $("#participateready").hide();
         $("#step1").show();
-        var database = firebase.database();
-        var apology = database.ref("apology");
+    });
+    loadApology();
 
-var id = "-KWysaoVRjzT0rtAsckU";
-var ref = database.ref("apology/" + id);
-ref.on("value", gotOne);
-     
-function gotOne(apology) {
-  var apology = apology.val();
-    console.log(apology);
-    $("#apologyprompt").append(apology.text);
-}
-          
-});
-
-// when the user clicks submit after writing a forgiveness, preventdefault prevents the page from refreshing
 $("#forgivenessform").submit(function(e){
-      e.preventDefault()
-      $("#step2").show();
-       $("#step1").hide();
-        $("#participateready").hide();
-    
-var database = firebase.database();
-    var forgiveness = database.ref('forgiveness');
-    var timestamp= Date();
-    var forgivenesstext = {
-        text: $("#message").val(),   
-        approved: false,
-        time: timestamp
-        }; 
-    console.log (forgivenesstext)
-    forgiveness.push(forgivenesstext);
-    forgiveness.push(forgivenesstext, finished);
+    e.preventDefault()
+    $("#step1").hide();
+    $("#participateready").hide();
+    forgive($("#message").val());
 });
 
-// when the user clicks submit after writing an apology, preventdefault prevents the page from refreshing
 $("#apologyform").submit(function(e){
     e.preventDefault() 
-    $("#finished").show();
     $("#step2").hide();
     $("#step1").hide();
     $("#participateready").hide;
-    var database = firebase.database();
-  
-
-    var apology = database.ref('apology');  
-    var timestamp = Date();
-    console.log(timestamp);
-    var apologytext = {
-        text: $("#message2").val(), 
-        approved: false,
-        time: timestamp
-    
-        
-        }; 
-    console.log (apologytext);
-    apology.push(apologytext);
-    apology.push(apologytext, finished);
-var x= date();
-    consolelog(x);
+    apologize($("#message2").val());
 });
 
-//var timeStamp = Math.floor(Date.now() / 1000);
-//console.log(timestamp);
 
+});
 
  
